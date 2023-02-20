c41b6d931713271b2e529a03d1c76bb3ac247588
Viktar <viktarbrutsis@gmail.com>
2023-02-20 11:18:36 +0300

    feat: game done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,26 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.number = 0;
     }
-
+    //бинарный поиск - определить середину и посмотреть где guess число
     guess() {
-
+        this.number = Math.round((this.min + this.max) / 2);
+        return this.number;
     }
-
+    //загаданное число меньше, чем середина
+    //тогда середина становитсямакс
     lower() {
-
+        this.max = this.number;
+        this.guess();
     }
-
+    //загаданное число больше, чем середина
+    //тогда середина становится мин
     greater() {
-
+        this.min = this.number;
+        this.guess();
     }
 }
 
